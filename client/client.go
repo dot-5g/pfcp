27c304fc6d48f4b4dcdabe29f377834bdd57d9bf
Guillaume Belanger <guillaume.belanger27@gmail.com>
2023-12-18 20:18:47 -0500

    chore: reorganizes files based on client/server structure

--- client/client.go
+++ client/client.go
@@ -1,15 +1,37 @@
-package pfcp
+package client
 
 import (
 	"encoding/binary"
 	"log"
 	"time"
+
+	"github.com/dot-5g/pfcp/network"
 )
+
+type Pfcp struct {
+	ServerAddress string
+	Udp           network.UdpSender
+}
 
 type RecoveryTimeStamp time.Time
 
 type HeartbeatRequest struct {
 	RecoveryTimeStamp RecoveryTimeStamp
+}
+
+type HeartbeatResponse struct {
+	RecoveryTimeStamp RecoveryTimeStamp
+}
+
+func New(ServerAddress string) *Pfcp {
+
+	udpClient, err := network.NewUdp(ServerAddress)
+	if err != nil {
+		log.Printf("Failed to initialize UDP client: %v\n", err)
+		return nil
+	}
+
+	return &Pfcp{ServerAddress: ServerAddress, Udp: udpClient}
 }
 
 func (pfcp *Pfcp) sendPfcpMessage(header PFCPHeader, payload []byte, messageType string) error {
--- client/client_test.go
+++ client/client_test.go
@@ -1,9 +1,9 @@
-package pfcp_test
+package client_test
 
 import (
 	"testing"
 
-	"github.com/dot-5g/pfcp"
+	"github.com/dot-5g/pfcp/client"
 )
 
 type MockUdpSender struct {
@@ -14,14 +14,14 @@ func (m *MockUdpSender) Send(msg []byte) error {
 	return m.SendFunc(msg)
 }
 
-func TestSendHeartbeatRequest(t *testing.T) {
+func TestGivenPfcpWhenSendHeartbeatRequestThenNoError(t *testing.T) {
 	mockSender := &MockUdpSender{
 		SendFunc: func(msg []byte) error {
 			return nil
 		},
 	}
 
-	pfcpClient := pfcp.NewPfcp("127.0.0.1:8805")
+	pfcpClient := client.New("127.0.0.1:8805")
 	pfcpClient.Udp = mockSender
 
 	err := pfcpClient.SendHeartbeatRequest()
